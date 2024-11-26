d8c590bb47ab62160481d511cbae9e9b5e1c7eea
AlanDaniel123 <Perezperez9385@gmail.com>
2024-11-26 16:52:46 -0400

    Add files via upload

---
+++ Periodico escolar/main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.news-cards-bachata', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.btn-links', { delay: 500 });
+ScrollReveal().reveal('h2', { delay: 500 });
+ScrollReveal().reveal('h1', { delay: 500 });
+ScrollReveal().reveal('video', { delay: 500 });
